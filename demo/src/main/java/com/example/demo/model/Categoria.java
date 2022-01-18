eb5d726bec480ad5424bc63118a4b13403c9afd5
Jeiseel Rodrigues <jeiseel.rodrigues@dce.ufpb.br>
2022-01-18 15:36:22 -0300

    Create models category and products

---
+++ demo/src/main/java/com/example/demo/model/Categoria.java
@@ -0,0 +1,57 @@
+package com.example.demo.model;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "codigo")
+	private Long codigo;
+
+	@Column(name = "nome")
+	private String name;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(codigo, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(codigo, other.codigo) && Objects.equals(name, other.name);
+	}
+
+}
